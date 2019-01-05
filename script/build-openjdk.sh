34c1d1da794253dd800f3ea3bc9815476340cf5b
Paweł Żalejko <p.zalejko@gmail.com>
2019-01-05 23:30:53 +0100

    Update build-openjdk.sh

--- script/build-openjdk.sh
+++ script/build-openjdk.sh
@@ -2,7 +2,6 @@
 
 mkdir source
 wget https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz -P source/boot-jdk
-
 git clone https://github.com/AdoptOpenJDK/openjdk-jdk11u.git source/jdk
 
 cd docker
