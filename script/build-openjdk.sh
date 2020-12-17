85934626f0d41d65696fe9e05605a95981315a34
Pawel Zalejko <p.zalejko@gmail.com>
2020-12-17 23:12:26 +0100

    updated scripts

--- script/build-openjdk.sh
+++ script/build-openjdk.sh
@@ -4,6 +4,8 @@ mkdir source
 wget https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz -P source/boot-jdk
 git clone https://github.com/AdoptOpenJDK/openjdk-jdk11u.git source/jdk
 
+dir=$(PWD)
+
 cd docker
 docker build -t my-openjdk-builder .
-docker run -it -v /home/pawel/dev/test-openjdk/diy-openjdk/source:/opt/jdk-source my-openjdk-builder:latest
+docker run -it -v $dir/source:/opt/jdk-source my-openjdk-builder:latest
